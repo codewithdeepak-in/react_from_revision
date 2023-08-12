1cbbad57535112c4023822e85cd671771e88ebbc
Deepak Chaudhary <101586061+Deepak-Chaudhary20@users.noreply.github.com>
2023-08-12 09:35:38 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,45 @@
-// Here i created simple arrow function in javascipt 
-
+import { useState } from 'react';
 
 const App = () => {
- 
+
+    const [list, setList] = useState([12,31,123]);    
+
+    const createList = () => {
+        return list.map((item, key) => (
+          <li key={key}>{item}</li>
+        ))
+    }
+
+    const addNumber = () => {
+        setList([...list, 
+                Math.floor(Math.random() * 100)
+        ])
+
+    }
+    let a = Math.floor(Math.random() * 255)
+    let b = Math.floor(Math.random() * 255)
+    let c = Math.floor(Math.random() * 255)
+    
+    const removeNumber = () => {
+        const listarray = [...list];
+
+        listarray.pop();
+        setList(listarray);
+    }
+
+
     return (
-        // everything written inside return is not html but it's JSX 
         <div>
-            <h3>Hello World</h3>
+            <h1>Hello World!</h1>
+            {createList()}
+
+            <button onClick={addNumber} > Add</button>
+            <button onClick={removeNumber} > Remove</button>
+            <br />
+            <br />
+            <div style={{height: '100px',backgroundColor: `rgba(${a}, ${b}, ${c})`}}></div>
         </div>
     );
-
 }
 
 export default App;
